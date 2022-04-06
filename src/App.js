3945a0a704f39871567f536247b5a06da9584ee5
LauraBenavidesdlrv <ldbenavidesr@unal.edu.co>
2022-04-06 07:24:13 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+ //Using state Hooks
+  const [city, setCity] = useState("Vancouver");
+  const [country, setCountry] = useState("CA");
+  const [data, setData] = useState({});
+
+  
+
+  let uploaded = true
+  //Search
+  useEffect(()=>{
+    const searching  = async () => {
+      //API conection
+      const url = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=9deb3cccedf15a1276b9a6818a5d3718&units=metric`);
+      if (uploaded){
+        setData(await url.json());
+        console.log(data)
+      }
+      return () => {
+        uploaded = false;
+      }
+    }
+      searching();
+  }, [city,country]);
+
+    const sub = (e) => {
+      e.preventDefault();
+      setCity("")
+      setCountry("")
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="wrapper">
+        <form onSubmit={sub} className="search">
+          <input 
+          type="text" 
+          placeholder="City name, ex:Vancouver"
+          required
+          name="searchCity"
+          value={city}
+          onChange={(event) => setCity(event.target.value)}
+          />
+          <input 
+          type="text"
+          placeholder="Country code, ex:CA"
+          required
+          name="searchCountry"
+          value={country}
+          onChange={(event) => setCountry(event.target.value)}
+          />
+          <button type="button" onClick={sub}>Reset</button>
+        </form>
+        <div className="info">
+          <div className="location">
+            <p>{data.name}, {country}</p>
+          </div>
+          <div className="temp1">
+          {data.main ? <h1>{data.main.temp.toFixed()}°C <i className="bi bi-cloud-sun-fill"></i></h1> : null}
+            
+          </div>
+        </div>
+        <div className="weather">
+          <div className="feels topic">
+            {data.main ? <h3>{data.main.feels_like}°C</h3> : null}
+            <p>Feels Like <i className="bi bi-person-fill"></i></p>
+            
+          </div>
+          <div className="humidity topic">
+            {data.main ? <h3>{data.main.humidity}%</h3> : null}
+            <p>Humidity <i className="bi bi-moisture"></i></p>
+          </div>
+          <div className="speed topic">
+          {data.main ? <h3>{data.wind.speed}m/s</h3> : null}
+            <p>Wind Speed <i className="bi bi-wind"></i> </p>
+            
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
